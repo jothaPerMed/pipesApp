1b8c949cb2fe042e414b298f52461b612717a734
jgperea <josegpermed@gmail.com>
2023-11-08 12:05:01 +0100

    SlicePipe

--- src/app/product/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/product/pages/uncommon-page/uncommon-page.component.ts
@@ -20,4 +20,28 @@ export class UncommonPageComponent {
     this.name='Melisa';
     this.gender='female';
   }
+
+
+  //i18nPlural
+  public clients :string[]= [
+    'Maria',
+    'Juan',
+    'Pedro',
+    'Ana',
+    'Cristina',
+    'Alberto'
+  ];
+public clientsMap ={
+  '=0': 'No tenemos ningun cliente esperando.',
+  '=1':'tenemos un cliente esperando.',
+  '=2': 'tenemos 2 clientes esperando.',
+  'other':'tenemos # clientes esperando.'
+
+}
+
+  public deletClient(){
+    this.clients.shift();
+  }
+
+
 }
