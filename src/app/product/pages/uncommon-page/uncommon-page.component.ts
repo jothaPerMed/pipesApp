11ac6f2caf235c5323d3d9a8dfe885d9b45250df
jgperea <josegpermed@gmail.com>
2023-11-08 12:26:41 +0100

    AsyncPipe

--- src/app/product/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/product/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -52,5 +53,19 @@ public clientsMap ={
     address:'Ottawa Canada'
   }
 
+
+  // Async Pipe
+
+
+  public myObservable = interval(2000).pipe(
+    tap(value=> console.log('tap:',value))
+  )
+
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve('tenemos otra promesa.')
+    },3000)
+  })
 
 }
