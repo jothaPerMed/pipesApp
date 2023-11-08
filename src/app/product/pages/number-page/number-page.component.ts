ff6abdb7c500db2969089a381f82bb520648d6a1
jgperea <josegpermed@gmail.com>
2023-11-08 09:05:53 +0100

    generar rutas con carga perezosa

---
+++ src/app/product/pages/number-page/number-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-number-page',
+  templateUrl: './number-page.component.html',
+  styleUrls: ['./number-page.component.css']
+})
+export class NumberPageComponent {
+
+}
